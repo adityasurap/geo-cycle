f1d9dd2d819f82172de869e9a8c9bf8d2b39e03f
Aditya Surapaneni <asurap1426@gmail.com>
2025-02-17 13:09:59 -0800

    Added bike stations to the map.

--- map.js
+++ map.js
@@ -8,3 +8,78 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18
 });
+
+map.on('load', () => {
+    const svg = d3.select('#map').select('svg');
+    let stations = [];
+    let circles; // Declare circles outside of the data load scope
+
+    function getCoords(station) {
+        const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+        const { x, y } = map.project(point);
+        return { cx: x, cy: y };
+    }
+
+    function updatePositions() {
+        circles
+            .attr('cx', d => getCoords(d).cx)
+            .attr('cy', d => getCoords(d).cy);
+    }
+
+    const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json'
+    d3.json(jsonurl).then(jsonData => {
+        stations = jsonData.data.stations;
+
+        // Define circles after the stations are loaded
+        circles = svg.selectAll('circle')
+            .data(stations)
+            .enter()
+            .append('circle')
+            .attr('r', 5)
+            .attr('fill', 'steelblue')
+            .attr('stroke', 'white')
+            .attr('stroke-width', 1)
+            .attr('opacity', 0.8);
+
+        updatePositions(); // Call updatePositions after circles are created
+    }).catch(error => {
+        console.error('Error loading JSON:', error);
+    });
+
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 3,
+            'line-opacity': 0.6
+        }
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 3,
+            'line-opacity': 0.6
+        }
+    });
+
+    map.on('move', updatePositions);
+    map.on('zoom', updatePositions);
+    map.on('resize', updatePositions);
+    map.on('moveend', updatePositions);
+});
